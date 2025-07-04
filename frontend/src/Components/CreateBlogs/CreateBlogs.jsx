5dc9b44ef60ef6e3866d55841f2ea2d729684956
Nitish <nitish425689@gmail.com>
2025-07-04 14:48:49 +0530

    first commit

---
+++ frontend/src/Components/CreateBlogs/CreateBlogs.jsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./CreateBlogs.css";
+import upload_area from '../assets/upload_area.svg'
+import Quill from "quill";
+
+const CreateBlogs = () => {
+
+  const editorRef = useRef(null);
+   const quillRef = useRef(null);
+
+ const [createBlog, setCreateBlog] = useState({
+    title: '',
+    content:'',
+    image:''
+ })
+  const [image, setImage] = useState(false);
+  const [content, setContent] = useState('');
+  
+   const imageHandler = (e) => {
+        setImage(e.target.files[0])
+     }
+ 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(createBlog)
+     let responseData;
+         const content = quillRef.current.root.innerHTML;
+         let blog = {
+             ...createBlog,
+             content,
+          };
+
+        let formData = new FormData()
+        formData.append('blogimage', image);
+
+        await fetch('http://localhost:5000/api/v1/user/upload/blogimage', {
+            method: 'POST',
+            headers: {
+                Accept:'application/json',
+            },
+            body: formData,
+        }).then((resp)=> resp.json()).then((data)=> {responseData = data})
+
+        if(responseData.success)
+        {
+          blog.blogThumbnail= responseData.image_url;
+          console.log(blog);
+          await fetch('http://localhost:5000/api/v1/user/upload/blog', {
+            method:'POST',
+            headers:{
+                Accept:'application/json',
+                'Content-Type':'application/json',
+                Authorization: `Bearer ${localStorage.getItem('token')}` 
+            },
+            body:JSON.stringify(blog),
+          }).then((resp)=>resp.json()).then((data)=>{
+             data.success?alert('blog Added'):alert('Failed')
+          })
+        }
+   
+    setCreateBlog({
+      title: '',
+    content:'',
+    image:''   
+    });
+    setImage(false);
+    setContent('');
+    if (quillRef.current) {
+  quillRef.current.setContents([]);
+}
+  };
+
+  useEffect(()=>{
+    if(!quillRef.current && editorRef.current){
+      quillRef.current = new Quill(editorRef.current, {theme: 'snow',
+        
+      })
+       quillRef.current.on('text-change', () => {
+      setContent(quillRef.current.root.innerHTML);
+    });
+    }
+  }, [])
+
+  return (
+    <div className="blog-creation-container">
+      <h2>Create a New Blog</h2>
+      <form className="blog-form" onSubmit={handleSubmit}>
+        
+        <label htmlFor="file-input">
+                <img src={image ? URL.createObjectURL(image) : upload_area} alt=""  className='addblog-thumbnail-img'/>
+            </label>
+            <input onChange={imageHandler} type="file" name='image' id='file-input' hidden />      
+        <label>
+          Title:
+          <input type="text" value={createBlog.title} name="title" onChange={(e) => setCreateBlog({...createBlog, [e.target.name]:e.target.value})}
+            required className="blog-input" placeholder="Enter blog title"
+          />
+        </label>
+
+        <div>
+          Content:
+         
+            <div className="blog-content-area" ref={editorRef}></div>
+        </div>
+ 
+
+        <button  type="submit" className="blog-submit-btn">  Create Blog </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateBlogs;
---
+++ frontend/src/pages/UserContext.jsx
@@ -0,0 +1,42 @@
+import React, { createContext, useState, useContext, useEffect } from "react";
+
+const UserContext = createContext();
+
+export const useUser = () => useContext(UserContext);
+
+export function UserProvider({ children }) {
+  const [user, setUser] = useState(null); // or initial user from localStorage/api
+
+  const fetchUser = async () =>{
+    const token = localStorage.getItem("token");
+    if(!token) return;
+  
+    try {
+      const res = await fetch('http://localhost:5000/api/v1/user/auth',{
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+       const data = await res.json();
+
+      if (data.success) {
+        setUser(data.user);
+      } else {
+        console.error("Auth failed:", data.message);
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchUser();
+  }, [])
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
